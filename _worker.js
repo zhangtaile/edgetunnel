f31f0f143eeecae7ac4392c06345f282c7578607
xxxfeifeifei <40308676+zhangtaile@users.noreply.github.com>
2025-09-06 07:37:16 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = 'sfjpojf3';
 let proxyIP = '';
 let DNS64Server = '';
 //let sub = '';
@@ -4568,3 +4568,4 @@ async function nginx() {
 	`
     return text;
 }
+
