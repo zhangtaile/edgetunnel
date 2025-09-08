ba7e490bfdc8ce7bdf2e5ac45d9ca2c3b36d4180
xxxfeifeifei <40308676+zhangtaile@users.noreply.github.com>
2025-09-08 14:52:34 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 ﻿
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = 'ewiruws';
 let proxyIP = '';
 let DNS64Server = '';
 //let sub = '';
@@ -7187,4 +7187,5 @@ function config_Html(token = "test", proxyhost = "") {
 </html>`;
 
     return html;
+
 }
